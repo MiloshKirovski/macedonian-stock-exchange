2a130dd80c7b244a5354116d0a450fe44ce0d676
Stefan Novkovski <137414598+stefannovkovski@users.noreply.github.com>
2024-11-08 21:05:32 +0100

    Updated filter 1

--- Homework 1/data_pipeline/filter1_fetch_codes.py
+++ Homework 1/data_pipeline/filter1_fetch_codes.py
@@ -3,15 +3,6 @@ from bs4 import BeautifulSoup
 
 
 def fetch_company_codes(url):
-    """
-        Fetches a list of company codes from the Macedonian Stock Exchange website.
-
-        Args:
-            url (str): The URL of the webpage to scrape. Defaults to the historical data page.
-
-        Returns:
-            List[str]: A list of unique company codes.
-    """
     try:
         response = requests.get(url)
         response.raise_for_status()
